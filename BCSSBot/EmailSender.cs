1846e327d7f570f748c1032b2725501feb4fcf72
Alfie Richards <arichardsdev@gmail.com>
2020-08-29 13:17:42 +0100

    Actual Files??

--- BCSSBot/EmailSender.cs
+++ BCSSBot/EmailSender.cs
@@ -1,4 +1,56 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace BCSSBot
 {
-  public class $CLASS$ {$END$}
+    public class EmailSender : IDisposable
+    {
+        private SmtpClient _smtpClient;
+        public EmailSender()
+        {
+            _smtpClient = new SmtpClient();
+        }
+
+        public void SendEmail(string recipient, string message, string subject)
+        {
+            _smtpClient.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            _smtpClient.Authenticate("bcssbot@gmail.com", "uxkhhtenqumalqyh");
+            var email = new MimeMessage()
+            {
+                Sender = MailboxAddress.Parse("bcssbot@gmail.com"),
+                To = {MailboxAddress.Parse(recipient)},
+                Subject = subject,
+                Body = new TextPart(TextFormat.Plain) {Text = message}
+            };
+            _smtpClient.Send(email);
+            _smtpClient.Disconnect(true);
+        }
+
+        public void SendEmails(string[] recipients, string[] messages, string[] subject)
+        {
+            _smtpClient.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            _smtpClient.Authenticate("bcssbot@gmail.com", "uxkhhtenqumalqyh");
+            for (int i = 0; i < recipients.Length; i++)
+            {
+                var email = new MimeMessage()
+                {
+                    Sender = MailboxAddress.Parse("bcssbot@gmail.com"),
+                    To = {MailboxAddress.Parse(recipients[i])},
+                    Subject = subject[i],
+                    Body = new TextPart(TextFormat.Plain) {Text = messages[i]}
+                };
+                _smtpClient.Send(email);
+            }
+            _smtpClient.Disconnect(true);
+        }
+        
+        public void Dispose()
+        {
+            _smtpClient.Disconnect(true);
+            _smtpClient.Dispose();
+        }
+    }
 }
--- BCSSBot/Program.cs
+++ BCSSBot/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace BCSSBot
 {
     class Program
@@ -7,6 +6,21 @@ namespace BCSSBot
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            using var emailSender = new EmailSender();
+            emailSender.SendEmail("arichardsdev@gmail.com", "Test", "Subject");
+
+            const int len = 10;
+            string[] strings = new string[len]; 
+            string[] recipients = new string[len]; 
+            string[] subjects = new string[len]; 
+            for (int i = 0; i < len; i++)
+            {
+                strings[i] = $"{i}";
+                subjects[i] = $"subject{i}";
+                recipients[i] = "arichardsdev@gmail.com";
+            }
+            
+            emailSender.SendEmails(recipients, strings,subjects);
         }
     }
 }
