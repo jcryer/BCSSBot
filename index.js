33f509c8a6cbe18af24cbd0f605df7cb95f2d8aa
Joseph Cryer <jcryer1234@gmail.com>
2020-08-30 14:58:36 +0100

    Secret codes so i put it to private oooOOooOoo

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ url = require('url');
 router = express.Router();
 
 router.get('/', async function(req, res) {
- // axios.post('http://localhost:3000/test2', {
- //   todo: 'Buy the milk'
- // });
+
+  console.log(req.query.code);
   console.log(req.query.state);
-  console.log(req.query.access_token);
   res.send("200 OK");
-  //res.send("Yay! home page :) - " + req.query.state);
+  axios.post('https://discord.com/api/oauth2/token', {
+    'client_id': 749611213406732370,
+    'client_secret': zTlQslYb63TTnfMRLlfBOSplsI3nlYby,
+    'grant_type': 'authorization_code',
+    'code': req.query.code,
+    'redirect_uri': 'http://bcss-su.herokuapp.com',
+    'scope': 'identify'
+  }).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+/*
+  axios.post('http://localhost:3000/test2', {
+    todo: 'Buy the milk'
+  });
+  res.send("Yay! home page :) - " + req.query.state);
+  */
 });
 
 router.get('/callback', async function(req, res) {
